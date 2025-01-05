dc37dc1d8a4680e938e6f93b4608389df7dff56b
Ehsan <ehsankholusi@gmail.com>
2025-01-05 11:26:38 +0330

    asp - authentication is working

--- TinyUrler_DotNetVersion/TinyUrler_DotNetVersion/Models/Context/TContext.cs
+++ TinyUrler_DotNetVersion/TinyUrler_DotNetVersion/Models/Context/TContext.cs
@@ -1,8 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace TinyUrler_DotNetVersion.Models.Context
 {
-    public class TContext : DbContext
+    public class TContext : IdentityDbContext<TAppUser>
     {
         public TContext(DbContextOptions<TContext> options) : base(options)
         {
